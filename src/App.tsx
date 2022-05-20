7ca33decb71f0c960445843be02a9ac37176bb87
Echowxsy <echowxsy@gmail.com>
2022-05-20 15:24:06 +0800

    feat: update terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import WebTerminal from "./Terminal";
-import "./App.css";
+import React from 'react';
+import WebTerminal from './Terminal';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <WebTerminal />
     </div>
   );
--- src/Terminal.tsx
+++ src/Terminal.tsx
@@ -1,15 +1,15 @@
-import { Terminal } from "xterm";
-import "xterm/css/xterm.css";
-import { useCallback, useEffect, useRef } from "react";
-import { AttachAddon } from "xterm-addon-attach";
-import axios from "axios";
+import { Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
+import { useCallback, useEffect, useRef } from 'react';
+import { AttachAddon } from 'xterm-addon-attach';
+import axios from 'axios';
 
-const socketURL = "ws://127.0.0.1:4000/socket/";
+const socketURL = 'ws://127.0.0.1:4000/socket/';
 function WebTerminal() {
   //初始化当前系统环境，返回终端的 pid，标识当前终端的唯一性
-  const initSysEnv = async (term: Terminal) =>
+  const initTerminal = async () =>
     await axios
-      .post("http://127.0.0.1:4000/terminal")
+      .post('http://127.0.0.1:4000/terminal')
       .then((res) => res.data)
       .catch((err) => {
         throw new Error(err);
@@ -20,24 +20,59 @@ function WebTerminal() {
       fontFamily: 'Menlo, Monaco, "Courier New", monospace',
       fontWeight: 400,
       fontSize: 14,
-      rows: 200,
+      rows: 24,
     });
     //@ts-ignore
-    term.open(document.getElementById("terminal"));
+    term.open(document.getElementById('terminal'));
     term.focus();
-    async function asyncInitSysEnv() {
-      const pid = await initSysEnv(term),
-        ws = new WebSocket(socketURL + pid),
-        attachAddon = new AttachAddon(ws);
+    // function connect(u:string) {
+    //   var ws = new WebSocket(u);
+
+    //   ws.onclose = function (e) {
+    //     console.log(
+    //       'Socket is closed. Reconnect will be attempted in 1 second.',
+    //       e.reason
+    //     );
+    //     setTimeout(function () {
+    //       connect(u);
+    //     }, 1000);
+    //   };
+
+    //   ws.onerror = function (err) {
+    //     ws.close();
+    //   };
+    //   return ws;
+    // }
+
+    async function asyncInitTerminal() {
+      const pid = await initTerminal();
+      const ws = new WebSocket(socketURL + pid);
+
+      // connect();
+      const attachAddon = new AttachAddon(ws);
       term.loadAddon(attachAddon);
     }
-    asyncInitSysEnv();
+
+    asyncInitTerminal();
     return () => {
       //组件卸载，清除 Terminal 实例
+      console.log(1);
+
       term.dispose();
     };
   }, []);
-  return <div id="terminal"></div>;
+
+  return (
+    <div id="main">
+      <div className="Command">
+        <button>安装Steamcmd</button>
+        <button>安装CS:GO Server</button>
+        <button>更新CS:GO Server</button>
+        <button>安装Soucemod</button>
+      </div>
+      <div id="terminal"></div>
+    </div>
+  );
 }
 
 export default WebTerminal;
